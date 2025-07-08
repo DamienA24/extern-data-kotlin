ab701bde2e5da376e878df891f5db1af94c577be
DamienA <alves.d24@laposte.net>
2025-07-08 09:10:00 +0200

    refactor(loginActivity): use viewModel with loginActivity

--- app/src/main/java/com/aura/ui/login/LoginActivity.kt
+++ app/src/main/java/com/aura/ui/login/LoginActivity.kt
@@ -3,9 +3,16 @@ package com.aura.ui.login
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.aura.databinding.ActivityLoginBinding
 import com.aura.ui.home.HomeActivity
+import kotlinx.coroutines.launch
+import viewModel.login.LoginViewModel
 
 /**
  * The login activity for the app.
@@ -17,6 +24,10 @@ class LoginActivity : AppCompatActivity()
    * The binding for the login layout.
    */
   private lateinit var binding: ActivityLoginBinding
+  /**
+   * The view model for the login activity.
+   */
+  private val loginViewModel: LoginViewModel by viewModels()
 
   override fun onCreate(savedInstanceState: Bundle?)
   {
@@ -25,17 +36,44 @@ class LoginActivity : AppCompatActivity()
     binding = ActivityLoginBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
-    val login = binding.login
-    val loading = binding.loading
+    setupInputListeners()
+    observeViewModel()
+    setupLoginButton()
+  }
 
-    login.setOnClickListener {
-      loading.visibility = View.VISIBLE
+  private fun setupInputListeners() {
+    binding.identifier.doOnTextChanged { text, _, _, _ ->
+      loginViewModel.setEmail(text.toString())
+    }
 
-      val intent = Intent(this@LoginActivity, HomeActivity::class.java)
-      startActivity(intent)
-
-      finish()
+    binding.password.doOnTextChanged { text, _, _, _ ->
+      loginViewModel.setPassword(text.toString())
     }
   }
 
+  private fun observeViewModel() {
+    lifecycleScope.launch {
+      repeatOnLifecycle(Lifecycle.State.STARTED) {
+        loginViewModel.isLoginFormValid.collect { isEnabled ->
+          binding.login.isEnabled = isEnabled
+          binding.login.visibility = if (isEnabled) View.VISIBLE else View.INVISIBLE
+        }
+      }
+    }
+  }
+
+  private fun setupLoginButton() {
+    binding.login.setOnClickListener {
+
+      if (binding.login.isEnabled) {
+        navigateToHome()
+      }
+    }
+  }
+
+  private fun navigateToHome() {
+    val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+    startActivity(intent)
+    finish()
+  }
 }
